8c5b8914fcc0770a771e08e71ebf1d23971ebf7b
kjgarza <kgarza@datacite.org>
2023-05-04 22:04:57 +0200

    temp hardcode

--- src/science_graph/api_factory.py
+++ src/science_graph/api_factory.py
@@ -1,14 +1,14 @@
 from .graphql_api import GraphQLAPI
 from .rest_api import RestAPI
-import config
+# import settings as config
 
 class APIFactory:
 
     @staticmethod
     def create_api(api_type):
         if api_type == 'graphql':
-            return GraphQLAPI(url=config.packg_config['pid_graph_api']['base_url'])
+            return GraphQLAPI(url="https://api.datacite.org/graphql")
         elif api_type == 'rest':
-            return RestAPI(base_url=config.packg_config['crossref_api']['base_url'])
+            return RestAPI(base_url="https://api.crossref.org/")
         else:
             raise ValueError(f"Unsupported API type: {api_type}")
