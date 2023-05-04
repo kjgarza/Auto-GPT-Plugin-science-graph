4fed8a86418bc57a63b16747500eba81b42645e7
kjgarza <kgarza@datacite.org>
2023-05-04 22:05:20 +0200

    remove printout

--- src/science_graph/settings.py
+++ src/science_graph/settings.py
@@ -1,8 +1,6 @@
 import pathlib
 import tomli
 
-print(pathlib.Path(__file__).parents[2])
-
 path = pathlib.Path(__file__).parents[2] / "pyproject.toml"
 with path.open(mode="rb") as fp:
     packg_config = tomli.load(fp)
