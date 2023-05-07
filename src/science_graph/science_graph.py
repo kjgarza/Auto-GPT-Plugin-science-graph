81269b3092b3b1fb1675a5a127972e615224a47b
kjgarza <kgarza@datacite.org>
2023-05-07 22:57:12 +0200

    new mthods

--- src/science_graph/science_graph.py
+++ src/science_graph/science_graph.py
@@ -1,6 +1,7 @@
 from .api_factory import APIFactory
-from .utils import reduce_reponse, safe_response
-import json
+from .utils import reduce_reponse, safe_response, flatted_graphql_response
+import requests
+
 from . import AutoGPTPluginScienceGraph
 
 plugin = AutoGPTPluginScienceGraph()
@@ -10,7 +11,13 @@ class ScienceGraph:
     def __init__(self):
         pass  # You can initialize any necessary attributes here
     
-    def _search_scholarly_works(self, keyword):
+    def _search_scholarly_works(self, keyword: str):
+        """Search any type of scholarly works by query
+        Args:
+            keyword (str): The keyword to search
+        Returns:
+            str: The response from the search in JSON format
+        """
         api_instance = APIFactory.create_api('graphql')
         response = api_instance.query(
             """
@@ -33,9 +40,32 @@ class ScienceGraph:
             """,
             {"keyword": keyword},
         )
-        return safe_response(response['works']['nodes'])
+        return safe_response(flatted_graphql_response(response['works']['nodes']))
     
-    def _search_journal_articles(self, keyword):
+    def _search_journal_articles(self, keyword: str):
+        """Search journal articles by query
+        Args:
+            keyword (str): The keyword to search
+        Returns:
+                str: The response from the search in JSON format
+        """
         api_instance = APIFactory.create_api('rest')
-        response = api_instance.query("works", params={"query": keyword})
+        response = api_instance.query("works", params={"query.description": keyword})
         return safe_response(reduce_reponse(response['message']['items']))
+
+
+    def _get_article_or_work(self, href: str):
+        """Return the results of a href resolved
+
+        Args:
+            href (str): The href to scrape
+
+        Returns:
+            str or list: The response from the page
+        """
+        try:
+            response = requests.get(href)
+            return response.text
+        except requests.exceptions.RequestException as e:
+            raise e
+        
